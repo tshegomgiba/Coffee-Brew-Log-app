2145cd8c4a253789561fbf3fc55aa0c7981f0953
Tshegofatso <tshegofatsononyane009@gmail.com>
2026-07-29 14:08:46 +0200

    tryimg netlify

--- backend/server.js
+++ backend/server.js
@@ -20,25 +20,13 @@ const initDB = async () => {
 const start = async () => {
   try {
     await initDB();
-    if (!process.env.VERCEL) {
-      const PORT = process.env.PORT || 5000;
-      app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`);
-      });
-    }
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
   } catch (error) {
     console.error(error);
   }
 };
 
 start();
-
-export default async function handler(req, res) {
-  try {
-    await initDB();
-    app(req, res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-}
